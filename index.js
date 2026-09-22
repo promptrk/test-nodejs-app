0330215dea8d89a3cd4f89b51fed64b14a1dc6df
Rohit K Manoj <rohitkmanoj15@gmail.com>
2026-09-22 10:26:46 +0530

    Added NodeJS index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/status", (req, res) => {
+	res.json({
+		status: "API is running"
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running at localhost:${PORT}`);
+});
